bf9353b9ad30d4a80572830f1fc82c0b4d90ff1c
emanb29 <eb@ethanbell.me>
2020-02-26 16:43:47 -0800

    Implement Entropy

---
+++ src/main/scala/me/ethanbell/balsam/Entropy.scala
@@ -0,0 +1,47 @@
+package me.ethanbell.balsam
+
+import java.security.MessageDigest
+
+import zio._
+import me.ethanbell.bitchunk.BitChunk
+
+object Entropy {
+
+  /**
+   * Creates an Entropy from a BitChunk, or returns an IllegalArgumentException if bs.n does not divide 32
+   * @param bs
+   * @return
+   */
+  def fromBitChunk(bs: BitChunk): IO[IllegalArgumentException, Entropy] =
+    if (bs.n % 32 != 0)
+      IO.fail(
+        new IllegalArgumentException(
+          "Cannot generate seeds for entropy whose length does not divide 32",
+        ),
+      )
+    else if (bs.n > 256)
+      IO.fail(
+        new IllegalArgumentException("Cannot generate seeds for entropy longer than 256 bits"),
+      )
+    else IO.succeed(Entropy(bs))
+  def apply(values: Seq[Int]): Entropy =
+    if (values.isEmpty) empty
+    else Entropy(values.map(BitChunk.apply).reduce(_ ++ _))
+
+  val empty = Entropy(BitChunk.empty)
+}
+
+case class Entropy private (bits: BitChunk) {
+  require(bits.n % 32 == 0, "Entropy must be constructed with multiples of 32 bits")
+  lazy val checksum: BitChunk =
+    MessageDigest
+      .getInstance("SHA-256")
+      .digest(bits.toBytes().toArray) // hash ENT
+      .map(BitChunk.apply)
+      .reduce(_ ++ _) // convert hash into a BitChunk
+      .take(bits.n / 32)
+  private lazy val concatenatedBits = bits ++ checksum
+  lazy val wordIndices: Seq[Int] = concatenatedBits
+    .grouped(11) // group into 11 bit chunks. These will be numbers in the range [0, 2047]
+    .map(_.toBigInt().toInt)
+}
--- src/main/scala/me/ethanbell/balsam/Impure.scala
+++ src/main/scala/me/ethanbell/balsam/Impure.scala
@@ -1,6 +1,6 @@
 package me.ethanbell.balsam
 
 /**
- * "Impure" functions exposing practical (ie, useful)
+ * "Impure" functions exposing practical (ie, useful) wrappers for pure behaviors of this library
  */
 protected[balsam] object Impure {}
